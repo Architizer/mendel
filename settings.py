8f513c0c9661c0318a9645969aa4505d1dfc7618
Andrew Westling <a@andrewwestling.com>
2016-05-06 15:18:08 -0400

    Remove /static from settings.py collectstatic config

--- settings.py
+++ settings.py
@@ -128,11 +128,6 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

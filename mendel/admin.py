5173cff8383d3ad44edba62b97231f5a9634b96e
Sean Auriti <sean@architizer.com>
2016-05-10 18:18:52 -0400

    closes #6

--- mendel/admin.py
+++ mendel/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 
-from mendel.models import Tag, Keyword
+from mendel.models import Keyword, Category, Document, Content, Review
 
-admin.site.register(Tag)
+
 admin.site.register(Keyword)
+admin.site.register(Category)
+admin.site.register(Document)
+admin.site.register(Content)
+admin.site.register(Review)
+
+
+
+
--- mendel/models.py
+++ mendel/models.py
@@ -1,26 +1,55 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-
-class Tag(models.Model):
-
-    def __unicode__(self):
-        return self.name
-
-    name = models.CharField(max_length=200, unique=True)
 
 
 class Keyword(models.Model):
-
-    def __unicode__(self):
-        return self.name + u' [' + self.tag_list + u']'
-
     name = models.CharField(max_length=200, unique=True)
-    is_stopword = models.BooleanField(default=False)
-    tags = models.ManyToManyField(Tag)
-    frequency = models.IntegerField(null=True)
 
-    @property
-    def tag_list(self):
-        return u'; '.join([tag['name'] for tag in self.tags.values()])
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+
+class Document(models.Model):
+    
+    CSV = "csv"
+    TXT = "txt"
+    
+    DOCUMENT_TYPES = (
+        ("CSV", CSV),
+        ("TXT", TXT),
+    )
+
+    of_type = models.CharField(max_length=10, choices=DOCUMENT_TYPES)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+
+class Content(models.Model):
+    document = models.ForeignKey(Document)
+    keyword = models.ForeignKey(Keyword)
+    position_from = models.IntegerField()
+    position_to = models.IntegerField()
+    text = models.TextField(blank=True, null=True)
+
+
+class Review(models.Model):
+    
+    PENDING = "pending"
+    APPROVED = "approved"
+    
+    STATUS_TYPES = (
+        ("pending", PENDING),
+        ("approved", APPROVED),
+    )
+
+    context = models.ForeignKey(Content)
+    keyword = models.ForeignKey(Keyword)
+    category = models.ForeignKey(Category)
+    user = models.ForeignKey(User, unique=True)
+    status = models.CharField(max_length=2, choices=STATUS_TYPES)
+
+

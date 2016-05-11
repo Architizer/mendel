eb0ca09292aef9ea515e5890647528e3b250fb8f
Andrew Westling <a@andrewwestling.com>
2016-05-11 13:01:27 -0400

    Add base template for "Login" view

--- mendel/static/src/app/index.route.js
+++ mendel/static/src/app/index.route.js
@@ -8,14 +8,21 @@
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
-        url: '/',
+      .state('main', {
+        url: '/main',
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+
+      .state('login', {
+        url: '/login',
+        templateUrl: 'app/login/login.html',
+        controller: 'LoginController',
+        controllerAs: 'login'
       });
 
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/main');
   }
 
 })();

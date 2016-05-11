a3873a9b3886b49dbfd6a97b344522589374d235
Andrew Westling <a@andrewwestling.com>
2016-05-11 12:38:43 -0400

    Add base template for "Main" view

--- mendel/static/src/app/main/main.controller.js
+++ mendel/static/src/app/main/main.controller.js
@@ -7,7 +7,28 @@
 
   /** @ngInject */
   function MainController() {
-    // var vm = this;
+    var vm = this;
 
+    vm.categories = [
+      'Aesthetic Concepts',
+      'Architect Name',
+      'Architectural Element',
+      'Architectural Style',
+      'Building Type',
+      'City',
+      'Color',
+      'Continent',
+      'Country',
+      'Finish',
+      'Firm Name',
+      'Manufacturer Name',
+      'Material',
+      'Performance Concepts',
+      'Price Concepts',
+      'Product Type',
+      'Room/Space Type',
+      'State',
+      'Unit'
+    ];
   }
 })();

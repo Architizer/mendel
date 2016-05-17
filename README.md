524b3714c19e665511c141e64e05635a63e3db66
Andrew Westling <a@andrewwestling.com>
2016-05-17 17:21:45 -0400

    Add support for .env file with `heroku local`

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Make sure you have Python [installed properly](http://install.python-guide.org).
 ```sh
 $ git clone git@github.com:Architizer/mendel.git
 $ cd mendel
+$ cp .env.example .env
 
 $ pip install -r requirements.txt
 
--- settings.py
+++ settings.py
@@ -22,10 +22,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: change this before deploying to production!
-SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') or False
 
 
 # Application definition

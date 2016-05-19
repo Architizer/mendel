e7e852cd8806b01760705abc9599e093f3147635
Andrew Westling <a@andrewwestling.com>
2016-05-18 22:19:07 -0400

    Prevent $resource from stripping trailing slashes from API calls

--- mendel/angular/src/app/index.config.js
+++ mendel/angular/src/app/index.config.js
@@ -6,12 +6,15 @@
     .config(config);
 
   /** @ngInject */
-  function config($logProvider, $locationProvider, toastrConfig) {
+  function config($logProvider, $locationProvider, $resourceProvider, toastrConfig) {
     // Enable log
     $logProvider.debugEnabled(true);
 
     // Enable HTML5 History Mode
     $locationProvider.html5Mode(true);
+
+    // Don't strip trailing slashes from $resource API calls
+    $resourceProvider.defaults.stripTrailingSlashes = false;
 
     // Set options third-party lib
     toastrConfig.allowHtml = true;

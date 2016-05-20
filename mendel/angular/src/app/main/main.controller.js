ce004b9aefb7642b4501e06dd65fcb0af06bf47e
Andrew Westling <a@andrewwestling.com>
2016-05-20 12:08:02 -0400

    update Context.get() function

--- mendel/angular/src/app/main/main.controller.js
+++ mendel/angular/src/app/main/main.controller.js
@@ -10,12 +10,15 @@
     var vm = this;
 
     // Get Context
-    Context.get({id: 1}, function (context) {
-
-      vm.context = context;
-      vm.context.front = '"...' + vm.context.text.substring(0, vm.context.position_from);
-      vm.context.back = vm.context.text.substring(vm.context.position_to, vm.context.text.length) + '..."';
-      vm.keyword = context.keyword;
+    Context.get({
+      id: 1
+    }, function (context) {
+        vm.context = context;
+        vm.context.front = '"...' + vm.context.text.substring(0, vm.context.position_from);
+        vm.context.back = vm.context.text.substring(vm.context.position_to, vm.context.text.length) + '..."';
+        vm.keyword = context.keyword;
+    }, function (error) {
+        toastr.error('There was an error:' + JSON.stringify(error), {timeOut: 5000});
     });
 
     // Get Categories

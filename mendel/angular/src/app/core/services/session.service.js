798286706aa2daa84fcf6c812fcc6b3091e3dddc
Andrew Westling <a@andrewwestling.com>
2016-05-28 17:19:45 -0400

    Create Session service

--- mendel/angular/src/app/core/services/session.service.js
+++ mendel/angular/src/app/core/services/session.service.js
@@ -3,16 +3,37 @@
 
   angular
     .module('static')
-    .service('Session', function () {
+    .service('Session', function ($http, $localStorage) {
 
-      this.create = function (sessionId, userId) {
-        this.id = sessionId;
-        this.userId = userId;
+      // Update Local Storage and Authorization Header with Token
+      var updateLocalStorage = function updateLocalStorage (token) {
+
+        $localStorage._mendelToken = token;
+
+        // Update HTTP Authorization Header
+        $http.defaults.headers.common['Authorization'] = 'Token ' + token;
       };
 
+      // Create Session
+      this.create = function (key, user) {
+
+        this.user = user;
+        this.token = key;
+
+        updateLocalStorage(this.token);
+
+        return this;
+      };
+
+      // Destroy Session
       this.destroy = function () {
-        this.id = null;
-        this.userId = null;
+
+        this.user = null;
+        this.token = null;
+
+        updateLocalStorage(this.token);
+
+        return this;
       };
 
     });

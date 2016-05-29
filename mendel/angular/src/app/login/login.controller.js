7d2d6ec4a1cd698cde58dbae32b5291534bddf32
Andrew Westling <a@andrewwestling.com>
2016-05-29 15:43:04 -0400

    Clean up Login controller

--- mendel/angular/src/app/login/login.controller.js
+++ mendel/angular/src/app/login/login.controller.js
@@ -6,23 +6,19 @@
     .controller('LoginController', LoginController);
 
   /** @ngInject */
-  function LoginController($rootScope, AUTH_EVENTS, AuthService) {
+  function LoginController(AuthService) {
     var vm = this;
 
-    vm.isAuthenticated = AuthService.isAuthenticated;
+    vm.submit = submitLogin;
 
     vm.credentials = {
       username: '',
       password: ''
     };
 
-    vm.submit = function submitLogin(credentials) {
+    function submitLogin(credentials) {
+      AuthService.login(credentials);
+    }
 
-      AuthService.login(credentials).then(function (user) {
-        $rootScope.$emit(AUTH_EVENTS.loginSuccess, user);
-      }, function () {
-        $rootScope.$emit(AUTH_EVENTS.loginFailed);
-      });
-    };
   }
 })();

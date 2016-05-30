bc5686a0a3029b74b3164aa7d4e3810712bd4441
Andrew Westling <a@andrewwestling.com>
2016-05-29 23:35:03 -0400

    (fix $templateCache to use 'mendel' as module name)

--- mendel/angular/gulp/build.js
+++ mendel/angular/gulp/build.js
@@ -20,7 +20,7 @@ gulp.task('partials', function () {
       collapseWhitespace: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'static',
+      module: 'mendel',
       root: 'app'
     }))
     .pipe(gulp.dest(conf.paths.tmp + '/partials/'));
--- mendel/angular/karma.conf.js
+++ mendel/angular/karma.conf.js
@@ -50,7 +50,7 @@ module.exports = function(config) {
 
     ngHtml2JsPreprocessor: {
       stripPrefix: conf.paths.src + '/',
-      moduleName: 'static'
+      moduleName: 'mendel'
     },
 
     logLevel: 'WARN',

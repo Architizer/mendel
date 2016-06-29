6b04cd83e368c5e355fc04b975ef6ab30105e8c0
Sean Auriti <sean@architizer.com>
2016-06-29 14:40:48 -0400

    MENDEL-101 Reform the API - when a user wants to fix previous category assignments, currently the front end does both calls (assign new contexts + delete old contexts). Need to make it so that the "delete" call happens on the back end, not the front end

--- mendel/views.py
+++ mendel/views.py
@@ -1,7 +1,44 @@
 from django.conf import settings
 from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from models import Context, Review, Keyword, Category
 
-# Create your views here.
+
 def index(request):
     context = {'DEBUG': settings.DEBUG}
     return render(request, 'index.html', context)
+
+ 
+class PostContext(APIView):
+
+    def post(self, request, id):
+        context = Context.objects.get(id=id)
+        counter = 0
+        keyword_proposed = context.keyword_given
+
+        if request.data.get('keyword_proposed'):
+            keyword_proposed, created = Keyword.objects.get_or_create(
+                name=request.data.get('keyword_proposed')['name']
+            )
+
+        existing_review_count = Review.objects.filter(context=context).count()
+        if not existing_review_count:
+            for category in request.data.get('categories'):
+                Review.objects.create(
+                    context=context, 
+                    category=Category.objects.get(id=category), 
+                    keyword_given=context.keyword_given, 
+                    keyword_proposed=keyword_proposed,
+                    user=request.user,
+                    status=Review.PENDING)
+                counter += 1
+
+        return Response({
+            "keyword_created": created, 
+            "keyword_proposed": keyword_proposed.name, 
+            "reviews_created": counter, 
+            "existing_review_count": existing_review_count
+            })
+

b367233d12dd6a9e9f88c970e55473eaa4037e49
Andrew Westling <a@andrewwestling.com>
2016-07-11 08:18:11 -0400

    Add login controller specs (WIP)

--- mendel/angular/src/app/login/login.controller.spec.js
+++ mendel/angular/src/app/login/login.controller.spec.js
@@ -1,7 +1,45 @@
 (function() {
   'use strict';
 
-  describe('controllers', function(){
+  describe('login', function(){
+
+    beforeEach(module('mendel'));
+
+    describe('controller', function () {
+
+      var ctrl, Session;
+
+      beforeEach(inject(function($controller, _Session_) {
+        ctrl = $controller('LoginController');
+        Session = _Session_;
+        
+        spyOn(ctrl, 'submit');
+      }));
+
+      it('has a credentials object with username and password', function () {
+
+        expect(ctrl.credentials.username).toBeDefined();
+        expect(ctrl.credentials.password).toBeDefined();
+      });
+
+      describe('submitLogin() function', function () {
+
+        it('exists', function () {
+
+          expect(ctrl.submit).toBeDefined();
+        });
+
+        it('logs a user in', function () {
+
+          ctrl.credentials.username = 'frank';
+          ctrl.credentials.password = 'lloydwright';
+
+          ctrl.submit(ctrl.credentials);
+
+          expect(ctrl.submit).toHaveBeenCalledWith(ctrl.credentials);
+        });
+      });
+    });
 
   });
 })();
